ab7ba70c47f57bead99306f166393b2578c4072d
朱志财 <zhuzhicai@tzdtz.com>
2018-12-25 13:44:30 +0800

    修改颜色

--- docs/.vuepress/components/index.vue
+++ docs/.vuepress/components/index.vue
@@ -49,7 +49,7 @@
           font-weight: 500;
         }
         &__desc {
-          color: #989898;
+          color: #666;
           height: 45px;
           overflow: hidden;
           text-overflow: ellipsis;
